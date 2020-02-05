ca1628e590f4e03f4f8dad844298e70ca8e289db
Gabriel Lievano <jglievano@wolfmango.com>
2020-02-04 16:17:01 -0800

    First commit.

---
+++ env.zsh
@@ -0,0 +1,24 @@
+export XDG_CONFIG_HOME=$HOME/.config
+
+export HISTSIZE=32768
+export HISTFILESIZE=${HISTSIZE}
+export SAVEHIST=4096
+export HISTCONTROL=ignoredups:erasedups
+
+export CLICOLOR=1
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US
+
+export LESS_TERMCAP_md="${yellow}"
+
+export EDITOR=vim
+export MANPAGER='less -X'
+
+export ANDROID_HOME=$HOME/android/sdk
+export ANDROID_SDK_ROOT=$HOME/android/sdk
+export ANDROID_AVD_HOME=$HOME/.android/avd
+export GOPATH=$HOME/go
+
+GPG_TTY=`tty`
+export GPG_TTY
---
+++ zplug.zsh
@@ -0,0 +1,17 @@
+if [ ! -d "$HOME/.zplug" ]; then
+	curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
+
+source ~/.zplug/init.zsh
+
+zplug "dracula/zsh", as:theme
+zplug "lib/history", from:oh-my-zsh
+zplug "lib/completion", from:oh-my-zsh
+zplug "lib/key-bindings", from:oh-my-zsh
+zplug "plugins/shrink-path", from:oh-my-zsh
+
+if ! zplug check; then
+	zplug install
+fi
+
+zplug load

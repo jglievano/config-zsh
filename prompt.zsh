73c4b40cf2e8054f8dbe275448852ed71e21c528
Gabriel Lievano <jglievano@gmail.com>
2020-03-10 13:28:04 -0700

    More grayish prompt.

--- prompt.zsh
+++ prompt.zsh
@@ -2,4 +2,4 @@ setopt prompt_subst
 autoload -U colors && colors
 
 NL=$'\n'
-PROMPT='%F{240}░░%F{228}%n%F{214}@%F{228}%m %F{166}$(shrink_path -f)${NL}%F{31}❯ %f'
+PROMPT='%F{240}░░%F{245}%n%F{240}@%F{245}%m %F{166}$(shrink_path -f)${NL}%F{31}❯ %f'

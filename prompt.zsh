b24f31809bbcd1dee498980075ec89bf32c2ed51
Gabriel Lievano <jglievano@gmail.com>
2020-04-13 11:07:28 -0700

    Better contrast for dark.

--- prompt.zsh
+++ prompt.zsh
@@ -2,4 +2,4 @@ setopt prompt_subst
 autoload -U colors && colors
 
 NL=$'\n'
-PROMPT='%F{240}░░%F{245}%n%F{240}@%F{245}%m %F{166}$(shrink_path -f)${NL}%F{31}❯ %f'
+PROMPT='%F{250}░░%F{253}%n%F{250}@%F{253}%m %F{166}$(shrink_path -f)${NL}%F{31}❯ %f'

8c0278793fcdccacfecef89044850cc4224d28ba
Gabriel Lievano <jglievano@gmail.com>
2020-04-16 16:35:01 -0700

    Standard prompt.

--- prompt.zsh
+++ prompt.zsh
@@ -2,4 +2,4 @@ setopt prompt_subst
 autoload -U colors && colors
 
 NL=$'\n'
-PROMPT='%F{250}░░%F{253}%n%F{250}@%F{253}%m %F{166}$(shrink_path -f)${NL}%F{31}❯ %f'
+PROMPT='%F{252}%m%F{31}:%F{166}$(shrink_path -f)%F{31}$ %f'
